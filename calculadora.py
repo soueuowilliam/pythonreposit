66cd349cd0595a37e8f6ccc614f3fa4d5c5a6be3
William Roberto Alves Nascimento <116693989+soueuowilliam@users.noreply.github.com>
2022-10-26 19:28:57 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,20 @@
 while True:
     print('Seja bem vindo a calculadora !!')
+    print('[1] Adição::       \n[2] Multiplicação::'
+        '\n[3] Divisão::      \n[4] Subtração::'
+        '\n[5] Sair do programa::')
+
+    menu = input('Selecione uma dessa opções, por favor: ')
+    print()
+
+    if menu == '5':
+        print('Obrigado por utilizar a calculadora.')
+        break
+
     num1 = int(input('Por favor, digite um valor: '))
     num2 = int(input('Agora, digite outro valor: '))
     print()
-
-    print('[1] Adição::       \n[2] Multiplicação::'
-        '\n[3] Divisão::      \n[4] Subtração::'
-        '\n[5] Novos números::\n[6] Sair do programa::')
-
-    menu = input('Selecione uma dessa opções, por favor: ')
-    print()
     
-    if menu == '5':
-        num1 = int(input('Digite um valor: '))
-        num2 = int(input('Digite outro valor: '))
-        print('[1] Adição::       \n[2] Multiplicação::'
-            '\n[3] Divisão::      \n[4] Subtração::'
-            '\n[5] Novos números::\n[6] Sair do programa::')
-
-    menu = input('Selecione uma dessa opções, por favor: ')
-    print()
-
     if menu == '1':
         print(f'O resultado é {num1 + num2}.')
         print()
@@ -33,7 +27,3 @@ while True:
     if menu == '3':
         print(f'O resultado é {num1 / num2}.')
         print()
-
-    if menu == '6':
-        print('Obrigado por ter utilizado.')     
-        break
